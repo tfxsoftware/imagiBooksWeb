a34355c3a0a7d82cebda31bb2b6c72c9c9ab553b
Matheus Franco De Sá Taveira <matheusfstaveira@gmail.com>
2024-06-01 23:49:12 -0300

    Arrumando Login

--- public/js/login.js
+++ public/js/login.js
@@ -1,18 +1,39 @@
+import { GoogleAuthProvider } from "firebase/auth";
+
 document.addEventListener('DOMContentLoaded', function() {
+    
     const loginForm = document.getElementById('form-login');
     const errorMessage = document.getElementById('error-message');
 
     loginForm.addEventListener('submit', function(event) {
         event.preventDefault();
+        const email = document.getElementById('username').value; 
+        const senha = document.getElementById('password').value;
         
-        const username = document.getElementById('username').value;
-        const password = document.getElementById('password').value;
-
-        if (username === 'admin' && password === '123') {
-            alert('Login bem-sucedido!');
-            window.location.href = 'perfil.html';
+        if (email.trim() === '' || senha.trim() === '') {
+            alert('Todos os campos devem ser preenchidos.');
+        } else if (!isValidEmail(email)) {
+            alert('Por favor, insira um endereço de e-mail válido.');
         } else {
-            errorMessage.textContent = 'Usuário ou senha inválidos.';
+            const auth = getAuth();
+            signInWithEmailAndPassword(auth, email, password)
+            .then((userCredential) => {
+                const user = userCredential.user;
+                alert('Login bem-sucedido!');
+                loginForm.reset();
+                localStorage.setItem('userId', data.userId);
+                window.location.href = '/perfil.html';
+            
+            })
+            .catch((error) => {
+                console.error('Erro ao processar o login:', error);
+                alert('Houve um erro ao processar seu login. Por favor, tente novamente mais tarde.');
+            });
         }
     });
+
+    function isValidEmail(email) {
+        const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        return re.test(email);
+    }
 });
